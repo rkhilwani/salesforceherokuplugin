a2f4972fb5329aea50b1fc47849b6f4e6e8741fe
monusingh3180@gmail.com <monusingh3180@gmail.com>
2017-02-21 08:40:34 +0000

    server commit

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var express=require('express');
 var app=express();
 
 var pg = require('pg');
-
+app.set('port', (process.env.PORT || 5000));
 pg.defaults.ssl = true;
-app.get("/api/contacts", function(req, res){
+app.get("/", function(req, res){
 pg.connect(process.env.DATABASE_URL, function(err, client) {
   if (err) throw err;
   console.log('Connected to postgres! Getting schemas...');
@@ -15,4 +15,7 @@ pg.connect(process.env.DATABASE_URL, function(err, client) {
       res.JSON(row);
     });
 });
+});
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
