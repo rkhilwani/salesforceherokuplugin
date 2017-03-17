a8bcfcaec4f326f8604712042c4ef306d0662525
nishantnishant <monusingh3180@gmail.com>
2017-03-17 15:39:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,8 +9,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 var url = require('url');
 console.log(process.env.DATABASE_URL);
-console.log(process.env.DATABASE_URL);
-console.log(process.env.DATABASE_URL);
 var params = url.parse(process.env.DATABASE_URL);
 var auth = params.auth.split(':');
 
@@ -29,49 +27,21 @@ pg.defaults.ssl = true;
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-/*var config = {
-  user: 'byemztchoixznv', //env var: PGUSER
-  database: 'd6qh8k2rck35v8', //env var: PGDATABASE
-  password: '35b5fd10b572744d4018dfc72c1856ba9a2118d5233bd7d521e4231ff7aea9bf', //env var: PGPASSWORD
-  host: 'ec2-23-21-220-23.compute-1.amazonaws.com', // Server hosting the postgres database
-  port: 5432, //env var: PGPORT
-  max: 10, // max number of clients in the pool
- // idleTimeoutMillis: 100000, // how long a client is allowed to remain idle before being closed
-};
-*/
-//var pool1 = pgp('postgres://oprvfmfrktmuim:9db871afbdbf2f8bd1339d53de02359022e7ef5fb58392230d3a99cf32b63d48@ec2-54-204-32-145.compute-1.amazonaws.com:5432/d4q2qo2gph5otk');
-//var pool1=pgp(config);
 var pool = new pg.Pool(config);
-/*
-app.get("/", function(req, res){
-	pool.connect(function(err, client) {
-	if (err) throw err;
-	console.log('Connected to postgres! Getting schemas...');
 
-  
-  client
-    .query('SELECT AxtriaSalesIQTM__Client_Position_Code__c FROM salesforceorg2.AxtriaSalesIQTM__Position__c limit 1;')
-    .on('row', function(row) {
-      console.log(JSON.stringify(row));
-	  res.json(row);
-    });
-  console.log('nishant');
-});
-});
-*/
 app.post("/", function(req, res){
 pool.connect(function(err,client,done) {
  /* if (err){
   //console.log(err);
   throw err;
   }*/
-  console.log('Connected to postgres! Getting schemas...');
+  	console.log('Connected to postgres! Getting schemas...');
   //var param=req.params.id;
   //var param2=param;
   //var text;
-  console.log(req.body);
-  var sfdcid=req.body.Business_Rule_Change_request_sfid;
-  var bussinessRuleType=req.body.bussinessRuleType;
+  	console.log(req.body);
+  	var sfdcid=req.body.Business_Rule_Change_request_sfid;
+  	var bussinessRuleType=req.body.bussinessRuleType;
  // console.log(req.body.tempobj);
 	console.log(sfdcid);
 	console.log(sfdcid.length);
@@ -99,7 +69,7 @@ pool.connect(function(err,client,done) {
 
 	client.query("select sfdcbusinessrule.BusinessRuleExecute($1,$2)",[sfdcid,bussinessRuleType]);
 	//done();
-	//pool.end();
+	pool.end();
 	res.status(200).send('Connection Closed');
 
 });	
@@ -113,6 +83,7 @@ pool.on('error', function (err, client) {
   console.log('Population completed');
 });	
 
+//Method For deletion Starts Here
 
 app.post("/delete", function(req, res){
 pool.connect(function(err, client,done) {
@@ -153,8 +124,11 @@ pool.connect(function(err, client,done) {
   console.log('Population completed');
 });	
 	
-	
-    //res.write('Population Completed');
+
+
+
+
+//res.write('Population Completed');
 	//send image
 	//res.end();
 	//var resp = client.query("SELECT Name from salesforceorg2.AxtriaSalesIQTM__Team_Instance_Account__c where AxtriaSalesIQTM__Team_Instance__c =$1 limit 1",[param]);
@@ -197,6 +171,23 @@ pool.connect(function(err, client) {
     //});
 	
   console.log('Population completed');
+});
+});
+*/
+/*
+app.get("/", function(req, res){
+	pool.connect(function(err, client) {
+	if (err) throw err;
+	console.log('Connected to postgres! Getting schemas...');
+
+  
+  client
+    .query('SELECT AxtriaSalesIQTM__Client_Position_Code__c FROM salesforceorg2.AxtriaSalesIQTM__Position__c limit 1;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+	  res.json(row);
+    });
+  console.log('nishant');
 });
 });
 */
